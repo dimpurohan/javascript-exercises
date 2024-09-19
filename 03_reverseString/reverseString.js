363ae54531502b27a5799ae93e5dbcb1d8b7fb94
Rohan Rajput <rohanraaj30@gmail.com>
2024-09-19 21:12:46 +0530

    updated code

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    // Split the string into an array of characters
+    const charArray = string.split('');
+    
+    // Reverse the array
+    const reversedArray = charArray.reverse();
+    
+    // Join the array back into a string
+    const reversedString = reversedArray.join('');
+    
+    return reversedString;
 };
 
 // Do not edit below this line
